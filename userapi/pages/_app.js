8b59a4f8e3ebe5af1fedc2182eceaee4364f25c8
marcolap13 <mla.marco13@gmail.com>
2023-12-27 16:54:27 +0100

    feat: add redis functionnality
    transition from mongodb to redis

--- userapi/pages/_app.js
+++
@@ -1,6 +0,0 @@
-// pages/_app.js
-import '../styles/global.css' // Adjust the path to your global styles accordingly
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
