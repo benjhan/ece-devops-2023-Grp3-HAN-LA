76ec35f54033ac32fa7ed2d7da1ad8441925922c
marcolap13 <mla.marco13@gmail.com>
2023-12-29 15:29:12 +0100

    fix: userapi_redis port

--- userapi_with_REDIS_DOCKER/src/dbClient.js
+++ userapi_with_REDIS_DOCKER/src/dbClient.js
@@ -1,17 +1,22 @@
 var redis = require("redis");
 const configure = require('./configure')
 
-const config = configure()
+const config = configure();
 var db = redis.createClient({
-  host: config.redis.host,
-  port: config.redis.port,
-  retry_strategy: () => {
-    return new Error("Retry time exhausted")
-  }
-})
+  host: 'redis',
+  port: 6380,
+  retry_strategy: (options) => {
+    if (options.error && options.error.code === 'CONNECTION_BROKEN') {
+      // La connexion est cassée, réessayez dans 5 secondes
+      return 5000;
+    }
+    // Retourne une erreur pour abandonner la reconnexion
+    return new Error('Retry time exhausted');
+  },
+});
 
 process.on('SIGINT', function() {
   db.quit();
 });
 
-module.exports = db
+module.exports = db;
