b69c077df776801ce1478933c00821b0937ab4bc
marcolap13 <mla.marco13@gmail.com>
2023-12-31 16:58:29 +0100

    feat: add istio

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,10 +1,23 @@
 var redis = require("redis");
 
 var db = redis.createClient({
-  host: process.env.REDIS_HOST || "mon-redis",
+  host: process.env.REDIS_HOST || "127.0.0.1",
   port: process.env.REDIS_PORT || 6379,
-  retry_strategy: () => {
-    return new Error("Retry time exhausted");
+  retry_strategy: function(options) {
+    if (options.error && options.error.code === "ECONNREFUSED") {
+      // End reconnecting on a specific error and flush all commands with a individual error
+      return new Error("The server refused the connection");
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands with a individual error
+      return new Error("Retry time exhausted");
+    }
+    if (options.attempt > 10) {
+      // End reconnecting with built-in error
+      return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
   }
 });
 
